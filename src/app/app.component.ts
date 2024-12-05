a8cabcd7af0504cba0165011e69c792c4bc9bf6d
r-rullo <r-rullo@logitud.fr>
2024-12-05 19:53:30 +0100

    feat: add highlight.io skd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { NgtCanvas } from 'angular-three';
-import { Experience } from './experience/experience.component';
+import {Component, isDevMode} from '@angular/core';
+import {NgtCanvas} from 'angular-three';
+import {Experience} from './experience/experience.component';
+
+import {H} from 'highlight.run';
+
+if (!isDevMode()) {
+  H.init('jgo9vv6g', {
+    environment: 'production',
+    networkRecording: {
+      enabled: true,
+      recordHeadersAndBody: true,
+      urlBlocklist: [
+        // TODO: insert full or partial urls that you don't want to record here
+      ],
+    },
+  });
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <ngt-canvas [sceneGraph]="sceneGraph" />
+    <ngt-canvas [sceneGraph]="sceneGraph"/>
   `,
-  host: { class: 'block h-dvh w-full' },
+  host: {class: 'block h-dvh w-full'},
   imports: [NgtCanvas],
 })
 export class AppComponent {
