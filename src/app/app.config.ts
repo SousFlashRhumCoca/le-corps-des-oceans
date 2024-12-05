fb491fd1ee3def3c157c3fd338e6f8f34322b69c
AWilhelmcopamaco <A.wilhelm@cop-amaco.com>
2024-12-05 23:24:26 +0100

    merge main into meteo_api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import {provideRouter, Routes} from '@angular/router';
-import {Experience} from './experience/experience.component';
 import {MeteoComponent} from './meteo/meteo.component';
+import {AnatomyComponent} from './anatomy/anatomy.component';
 
 const routes: Routes = [
-    {path:'Experience',component:Experience},
+    {path:'Anatomy',component:AnatomyComponent},
     {path:'Meteo', component:MeteoComponent}
 
 ]
