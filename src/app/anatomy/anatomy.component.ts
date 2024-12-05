264ced821ed7c1dd4233c6d6782e22ec315dd51c
SFZsfz <rogertheo12345678@gmail.com>
2024-12-05 22:52:50 +0100

    implement OrbitControls

--- src/app/anatomy/anatomy.component.ts
+++ src/app/anatomy/anatomy.component.ts
@@ -5,19 +5,23 @@ import {
     ElementRef,
     viewChild,
 } from '@angular/core';
-import { extend } from 'angular-three';
+import {extend, injectStore, NgtArgs} from 'angular-three';
 import { AmbientLight, DirectionalLight, Group, Object3D } from 'three';
-import { MTLLoader, OBJLoader } from 'three-stdlib';
+import {MTLLoader, OBJLoader, OrbitControls} from 'three-stdlib';
 
 @Component({
     standalone: true,
     template: `
         <ngt-group #group>
+            <ngt-orbit-controls *args="[camera(), glDomElement()]" />
+            <ngt-grid-helper />
+
             <!-- The object will be dynamically inserted here -->
         </ngt-group>
     `,
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [NgtArgs]
 })
 export class AnatomyComponent {
     private groupRef = viewChild.required<ElementRef<Group>>('group');
@@ -25,8 +29,12 @@ export class AnatomyComponent {
     private mtlLoader = new MTLLoader();
     private objLoader = new OBJLoader();
 
+    private store = injectStore();
+    protected camera = this.store.select('camera');
+    protected glDomElement = this.store.select('gl', 'domElement');
+
     constructor() {
-        extend({ Group, Object3D, AmbientLight, DirectionalLight });
+        extend({ Group, Object3D, AmbientLight, DirectionalLight, OrbitControls });
 
         this.mtlLoader.load('anatomy.mtl', (materials) => {
             materials.preload();
@@ -35,12 +43,10 @@ export class AnatomyComponent {
                 // Retrieve the group
                 const group = this.groupRef().nativeElement;
 
-                // Center the object
-
                 // Add the object to the group
                 group.add(object);
-                object.scale.set(15, 15, 15);
-                object.position.set(0, -12, 2);
+                object.scale.set(6, 6, 6);
+                object.position.set(0, -5, -4);
 
                 // Add ambient light
                 const ambientLight = new AmbientLight(0xffffff, 0.5);
