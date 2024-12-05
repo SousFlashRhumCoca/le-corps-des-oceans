47250ab8c5db311637222c726468da609c4dc928
r-rullo <r-rullo@logitud.fr>
2024-12-06 00:22:38 +0100

    commit du matin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,21 @@ import { NgtCanvas } from 'angular-three';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {AnatomyComponent} from './anatomy/anatomy.component';
 
+import {H} from 'highlight.run';
+
+if (!isDevMode()) {
+  H.init('jgo9vv6g', {
+    environment: 'production',
+    networkRecording: {
+      enabled: true,
+      recordHeadersAndBody: true,
+      urlBlocklist: [
+        // TODO: insert full or partial urls that you don't want to record here
+      ],
+    },
+  });
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
