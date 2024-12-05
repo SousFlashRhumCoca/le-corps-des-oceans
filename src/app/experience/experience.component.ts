328da91d045f2cd2d1a1017e57a020af000917c1
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2024-12-05 21:10:10 +0100

    Lumière squelette

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,17 +1,17 @@
-import {DOCUMENT} from '@angular/common';
+import { DOCUMENT } from '@angular/common';
 import {
-    ChangeDetectionStrategy,
-    Component,
-    CUSTOM_ELEMENTS_SCHEMA,
-    Directive,
-    ElementRef,
-    inject,
-    viewChild,
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Directive,
+  ElementRef,
+  inject,
+  viewChild,
 } from '@angular/core';
-import {extend, injectBeforeRender} from 'angular-three';
-import {Group, Object3D} from 'three';
-import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import { extend, injectBeforeRender } from 'angular-three';
+import { AmbientLight, DirectionalLight, Group, Object3D } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 @Directive({
     selector: '[cursorPointer]',
     standalone: true,
@@ -40,26 +40,35 @@ export class CursorPointer {
 export class Experience {
     private groupRef = viewChild.required<ElementRef<Group>>('group');
 
-    loader = new GLTFLoader();
+    private loader = new GLTFLoader();
 
     constructor() {
         // Étendre les classes nécessaires à Three.js
-        extend({Group, Object3D});
+        extend({ Group, Object3D, AmbientLight, DirectionalLight });
 
         // Charger le fichier OBJ
         this.loader.load('Splanchnology.glb', (object) => {
             // Récupérer le groupe
-            const group = this.groupRef().nativeElement as Group;
+            const group = this.groupRef().nativeElement;
 
             // Ajouter l'objet au groupe
             group.add(object.scene);
             object.scene.scale.set(2, 2, 2);
+
+            // Ajouter de la lumière ambiante
+            const ambientLight = new AmbientLight(0xffffff, 0.5);
+            group.add(ambientLight);
+
+            // Ajouter de la lumière directionnelle
+            const directionalLight = new DirectionalLight(0xffffff, 1);
+            directionalLight.position.set(5, 10, 7.5);
+            group.add(directionalLight);
         });
 
         // Faire tourner le groupe continuellement
-        injectBeforeRender(({delta}) => {
-            const group = this.groupRef().nativeElement as Group;
-            group.rotation.y += delta;
+        injectBeforeRender(({ }) => {
+            const group = this.groupRef().nativeElement;
+            // group.rotation.y += 0.01;
         });
     }
 }
