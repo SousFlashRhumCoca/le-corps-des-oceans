dfad4b53c9851d234913dd514a4b034c2b1e7333
r-rullo <r-rullo@logitud.fr>
2024-12-05 21:30:32 +0100

    feat: add highlight.io skd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { NgtCanvas } from 'angular-three';
 import { AnatomyComponent } from './anatomy/anatomy.component';
+
+import { H } from 'highlight.run';
+
+if (!isDevMode()) {
+  H.init('jgo9vv6g', {
+    environment: 'production',
+    networkRecording: {
+      enabled: true,
+      recordHeadersAndBody: true,
+      urlBlocklist: [
+        // TODO: insert full or partial urls that you don't want to record here
+      ],
+    },
+  });
+}
 
 @Component({
   selector: 'app-root',
